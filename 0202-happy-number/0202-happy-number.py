c74ea3aabc0935652a1994cc5d5bc15d049f0ee9
Tsigabu seyoum <146573736+Tsigabuwalker@users.noreply.github.com>
2026-01-14 17:06:37 +0300

    Time: 3 ms (41.94%), Space: 19.4 MB (7.85%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def sum_of_squares(num):
+            total = 0
+            while num > 0:
+                digit = num % 10
+                total += digit ** 2
+                num //= 10
+            return total
+
+        seen = set()
+
+        while n != 1:
+            if n in seen:
+                return False  # cycle detected
+            seen.add(n)
+            n = sum_of_squares(n)
+
+        return True
