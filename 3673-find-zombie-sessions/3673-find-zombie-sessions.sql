d2d846967546ce27bcc4f76eab08dcdf477b65a0
Tsigabu seyoum <146573736+Tsigabuwalker@users.noreply.github.com>
2026-01-23 16:22:17 +0300

    Time: 301 ms (65.88%), Space: 0B (100%) - LeetHub

---
+++ 3673-find-zombie-sessions/3673-find-zombie-sessions.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+SELECT 
+    session_id, 
+    user_id,
+    TIMESTAMPDIFF(MINUTE, MIN(event_timestamp), MAX(event_timestamp)) AS session_duration_minutes,
+    COUNT(CASE WHEN event_type = 'scroll' THEN 1 END) AS scroll_count
+FROM app_events
+GROUP BY session_id, user_id
+HAVING 
+    -- 1. Session duration more than 30 minutes
+    TIMESTAMPDIFF(MINUTE, MIN(event_timestamp), MAX(event_timestamp)) > 30
+    
+    -- 2. At least 5 scroll events
+    AND COUNT(CASE WHEN event_type = 'scroll' THEN 1 END) >= 5
+    
+    -- 3. Click-to-scroll ratio less than 0.20
+    AND (COUNT(CASE WHEN event_type = 'click' THEN 1 END) / 
+         COUNT(CASE WHEN event_type = 'scroll' THEN 1 END)) < 0.20
+    
+    -- 4. No purchases made during the session
+    AND COUNT(CASE WHEN event_type = 'purchase' THEN 1 END) = 0
+
+ORDER BY scroll_count DESC, session_id ASC;
