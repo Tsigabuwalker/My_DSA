cb180a8aa26978fb671fe82167dfbd550c7fffaa
Tsigabu seyoum <146573736+Tsigabuwalker@users.noreply.github.com>
2026-02-04 09:05:31 +0300

    Time: 27 ms (90.4%), Space: 21.2 MB (23.46%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,18 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+
+        count = Counter(hand)
+
+        for card in sorted(count):
+            while count[card] > 0:
+                for next_card in range(card, card + groupSize):
+                    if count[next_card] == 0:
+                        return False
+                    count[next_card] -= 1
+
+        return True
