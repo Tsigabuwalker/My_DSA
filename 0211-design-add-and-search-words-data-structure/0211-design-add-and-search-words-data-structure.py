b6a9aee53dfee36b04ef51a28df4aba722c42cd3
Tsigabu seyoum <146573736+Tsigabuwalker@users.noreply.github.com>
2026-02-06 09:31:15 +0300

    Time: 861 ms (86.46%), Space: 68.7 MB (22.34%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.is_end = True
+
+    def search(self, word: str) -> bool:
+        def dfs(index, node):
+            # If we've checked all characters
+            if index == len(word):
+                return node.is_end
+
+            ch = word[index]
+
+            # If the character is a dot, try all possibilities
+            if ch == '.':
+                for child in node.children.values():
+                    if dfs(index + 1, child):
+                        return True
+                return False
+            else:
+                if ch not in node.children:
+                    return False
+                return dfs(index + 1, node.children[ch])
+
+        return dfs(0, self.root)
