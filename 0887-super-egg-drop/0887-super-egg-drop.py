a5ac198471ec24f7a56e8438b2ec0c9f514c0731
Tsigabu seyoum <146573736+Tsigabuwalker@users.noreply.github.com>
2026-02-10 01:41:55 +0300

    Time: 0 ms (100%), Space: 19.3 MB (74.19%) - LeetHub

---
+++ 0887-super-egg-drop/0887-super-egg-drop.py
@@ -0,0 +1,10 @@
+class Solution:
+    def superEggDrop(self, k: int, n: int) -> int:
+        dp = [0] * (k + 1)
+        moves = 0
+        while dp[k] < n:
+            moves += 1
+            # Update dp from high to low to avoid using updated values in same iteration
+            for eggs in range(k, 0, -1):
+                dp[eggs] = dp[eggs] + dp[eggs - 1] + 1
+        return moves
