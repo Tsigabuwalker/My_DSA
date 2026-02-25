8908d41d8263ce3b110ad5538100830721ade377
Tsigabu seyoum <146573736+Tsigabuwalker@users.noreply.github.com>
2026-02-25 09:46:06 +0300

    Time: 1 ms (84.17%), Space: 20.9 MB (62.38%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, root):
+        def dfs(node):
+            if not node:
+                return (0, 0)
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+            
+            rob = node.val + left[1] + right[1]
+            not_rob = max(left) + max(right)
+            
+            return (rob, not_rob)
+        
+        return max(dfs(root))
