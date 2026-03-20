176ab832b13466d3c069c2f0148967d7314a8214
Tsigabu seyoum <146573736+Tsigabuwalker@users.noreply.github.com>
2026-03-20 05:54:10 -0700

    Time: 0 ms (100%), Space: 19.7 MB (18.12%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -0,0 +1,26 @@
+class Solution:
+    def verticalTraversal(self, root):
+        nodes = []
+
+        def dfs(node, row, col):
+            if not node:
+                return
+            
+            nodes.append((col, row, node.val))
+            dfs(node.left, row + 1, col - 1)
+            dfs(node.right, row + 1, col + 1)
+
+        dfs(root, 0, 0)
+
+        nodes.sort()
+
+        result = []
+        prev_col = float('-inf')
+
+        for col, row, val in nodes:
+            if col != prev_col:
+                result.append([])
+                prev_col = col
+            result[-1].append(val)
+
+        return result
