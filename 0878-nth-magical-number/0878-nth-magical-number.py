6621c8b3d5b6f633f5dbdfb6213e9dcf383f142d
Tsigabu seyoum <146573736+Tsigabuwalker@users.noreply.github.com>
2026-03-27 10:42:59 +0300

    Time: 0 ms (100%), Space: 19.5 MB (51.1%) - LeetHub

---
+++ 0878-nth-magical-number/0878-nth-magical-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def nthMagicalNumber(self, n: int, a: int, b: int) -> int:
+        def gcd(x, y):
+            while y:
+                x, y = y, x % y
+            return x
+        
+        lcm = a * b // gcd(a, b)
+        left, right = 1, n * min(a, b)
+        
+        while left < right:
+            mid = (left + right) // 2
+            count = mid // a + mid // b - mid // lcm
+            
+            if count < n:
+                left = mid + 1
+            else:
+                right = mid
+        
+        return left % (10**9 + 7)
